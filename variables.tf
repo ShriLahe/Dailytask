c57d65dfada68e453ddfca102c2504387ac648a9
Shree Master Repo <103722689+ShriLahe@users.noreply.github.com>
2023-04-04 16:11:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,13 +13,13 @@ variable "vpc_cidr_block" {
 
 variable "public_subnet_cidrs" {
   type = list
-  default = [ "10.0.1.0/24", "10.0.2.0/24" ]
+  default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
   description = "Public Subnet CIDR Values"
 }
 
 variable "private_subnet_cidrs" {
   type = list
-  default = [ "10.0.4.0/24", "10.0.5.0/24" ]
+  default = [ "10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24" ]
   description = "Private Subnet CIDR Values"
 }
 
