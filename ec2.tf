26c8bfa3bbc4ddc88d8e1ccd1cdf21a3bccb608b
Shree Master Repo <103722689+ShriLahe@users.noreply.github.com>
2023-04-21 09:51:27 +0530

    user data added
    user data added

--- ec2.tf
+++ ec2.tf
@@ -6,4 +6,19 @@ resource "aws_instance" "web_server" {
   subnet_id     = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ssh_access.id]
   associate_public_ip_address = true
+  
+  
+  user_data = <<-EOF
+        #!/bin/bash
+        sudo apt-get update -y
+        sudo apt-get install apache2 -y
+        sudo systemctl start apache2
+        sudo systemctl enable apache2
+        echo "<html><body><h1>Welcome to my website!</h1></body></html>" > /var/www/html/index.html
+        sudo systemctl restart apache2
+  EOF
+
+  tags = {
+    "Name" : "WebServerInstall"
+  }
 } 
