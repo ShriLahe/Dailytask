357f59a0390dc889bcecc1570c8ba738db51d3de
Shree Master Repo <103722689+ShriLahe@users.noreply.github.com>
2023-04-21 09:47:20 +0530

    public ip aasign true
    public ip aasign true

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "web_server" {
   ami           = "ami-0f8ca728008ff5af4"
   instance_type = "t2.micro"
   key_name      = "terraform-key"
+  
   subnet_id     = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [
-      aws_security_group.ssh_access.id
-  ]
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
+  associate_public_ip_address = true
+} 
